ca0ec4425bb9330803e0490ebd406ea81e1843f8
Vinodkumar koduru <vinodkuamar57799@gmail.com>
2025-08-03 20:04:53 +0530

    Zip file

--- url-shortener/app/utils.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def is_valid_url(url):
-    regex = re.compile(
-        r'^(https?://)'  
-        r'([\w.-]+)'   
-        r'(:\d+)?'    
-        r'(/[\w./?%&=\-]*)?$', re.IGNORECASE)
-    return re.match(regex, url) is not None
